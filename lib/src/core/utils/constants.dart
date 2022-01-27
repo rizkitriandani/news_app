8a6a2477ed76b7f40a194d00a44e24a2bab1668d
Rizki <rizki@bahasakita.co.id>
2022-01-27 14:32:40 +0700

    Done.

---
+++ lib/src/core/utils/constants.dart
@@ -0,0 +1,11 @@
+const String kMaterialAppTitle = 'Baca Berita';
+
+// API
+const String kBaseUrl = 'https://newsapi.org/v2';
+
+const String kApiKey = 'b1f37c86a2974fa79dfc82d5f8c16d43';
+
+// Database
+const String kArticlesTableName = 'articles_table';
+
+const String kDatabaseName = 'app_database.db';
